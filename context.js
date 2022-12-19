c7ba227d55da983bade73d74814490348449a0ba
vupn <vupham140199@gmail.com>
2022-12-19 07:59:32 +0700

    complete

--- context.js
+++ context.js
@@ -15,23 +15,23 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+eat(); // global don't have name attribute => undefined eat fish food
 
 
 /********************************* Scenario 2 *********************************/
-// nemo.eat = eat;
-// nemo.eat(); // ?
+nemo.eat = eat;
+nemo.eat(); // Nemo eat fish food
 
 
 /********************************* Scenario 3 *********************************/
-// nemo.eat = eat;
-// eat(); // ?
+nemo.eat = eat;
+eat(); // (global)undefined eat fish food
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+nemo.swim(); // Nemo swimming in the water
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim;
+swim(); // Error because the context is global
